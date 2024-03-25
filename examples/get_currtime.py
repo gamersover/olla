e1ee6faf6ba312b3c8a9d4f90e0e0cd815e4abe7
gamersover <cmathking@gmail.com>
2024-03-25 22:15:17 +0800

    init project

---
+++ examples/get_currtime.py
@@ -0,0 +1,15 @@
+from olla.agent.react_agent import ReActAgent
+from olla.core.llm import OpenAI
+from olla.core.function import CurrTime
+
+if __name__ == "__main__":
+    llm = OpenAI(
+        base_url="http://localhost:11434/v1",
+        model="openchat",
+        api_key="sk-none",
+    )
+    agent = ReActAgent(
+        llm=llm,
+        tools=[CurrTime()]
+    )
+    agent.run("what time is now in Shanghai")
